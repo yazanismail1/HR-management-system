2b1e88adf4c560a4cfc67a70ce488c6c2e625967
Yazan Alfarra <yalfarra@outlook.com>
2022-08-02 21:43:27 +0300

    doing the styling and object for task 08

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// Employees Objects //
-
-// Employee 01 //
-
-const firstEmployee = {
-    employeeId: 1,
-    fullName: a,
-    department: a,
-    level: s,
-    imageUrl: www,
-    salary: www,
-}
-
-
+'use strict';
 
 // Generate four digits random number //
+
+// General function to generate a random four digit number 
 
 function generateRandomId() {
     let randomId = Math.floor((Math.random() * 10000) - 1);
@@ -29,6 +18,14 @@ function generateRandomId() {
     } else if (randomId >= 10000){
         return randomId;
     }
+    return randomId;
+}
+
+// Generate Employee IDs for task 08
+
+function idNumber(add){
+    let startingID = 1000;
+    return startingID + add;
 }
 
 // Generate a random salary based on employee level //
@@ -56,28 +53,104 @@ function generateRandomSalary(employeeLevel) {
 
 // Calculating the net salary //
 
-function netSalary(employeeLevel) {
-    let salary = generateRandomSalary(employeeLevel);
-    let netSalary = Math.floor(salary*.925);
-    return console.log(netSalary);
+function afterTaxSalary(level) {
+    let salary = generateRandomSalary(level);
+    let netSalary = Math.floor(salary*0.925);
+    return netSalary;
 }
 
-netSalary("Junior");
+// Employees Objects //
 
-console.log("ehy");
+// Employee 01 
 
+const firstEmployee = {
+    employeeId: function(){return idNumber(0)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Ghazi Samer",
+    department: "Administration",
+    level: 'Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 02 
 
+const SecondEmployee = {
+    employeeId: function(){return idNumber(1)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Lana Ali",
+    department: "Finance",
+    level: 'Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 03 
 
+const thirdEmployee = {
+    employeeId: function(){return idNumber(2)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Tamara Ayoub",
+    department: "Marketing",
+    level: 'Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 04 
 
+const fourthEmployee = {
+    employeeId: function(){return idNumber(3)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Safi Walid",
+    department: "Administration",
+    level: 'Mid-Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 05
 
+const fifthEmployee = {
+    employeeId: function(){return idNumber(4)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Omar Zaid",
+    department: "Development",
+    level: 'Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 06
 
+const sixthEmployee = {
+    employeeId: function(){return idNumber(5)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Rana Saleh",
+    department: "Development",
+    level: 'Junior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Employee 07
 
+const seventhEmployee = {
+    employeeId: function(){return idNumber(6)}, // You can change the inner function to generateRandomId() to get a random ID 
+    fullName: "Hadi Ahmad",
+    department: "Finance",
+    level: 'Mid-Senior',
+    imageUrl: "www.google.com",
+    salary: function(){return afterTaxSalary(this.level)},
+}
 
+// Outputting employees objects //
 
-// CONVERT THE console.log() TO return //
+function outputtingEmployeesObjects(object){
+    let name = "Employee name: " + object.fullName;
+    let salary = "Employee Salary: " + object.salary();
+    console.log(name);
+    console.log(salary);
+}
+
+outputtingEmployeesObjects(firstEmployee);
+outputtingEmployeesObjects(SecondEmployee);
+outputtingEmployeesObjects(thirdEmployee);
+outputtingEmployeesObjects(fourthEmployee);
+outputtingEmployeesObjects(fifthEmployee);
+outputtingEmployeesObjects(sixthEmployee);
+outputtingEmployeesObjects(seventhEmployee);
